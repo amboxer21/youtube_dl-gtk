159d4ce8b72342b80afa8403068b229002d8b926
Anthony Guevara <amboxer21@gmail.com>
2015-11-09 12:45:46 -0500

    Added a basic write history to file function.

--- youtube_dl-gtk.rb
+++ youtube_dl-gtk.rb
@@ -3,6 +3,10 @@
 require 'gtk2'
 require 'fileutils'
 
+# Envirenment variables
+$baseDir  = ENV['HOME']
+$musicDir = "#{$baseDir}/Music"
+
 window = Gtk::Window.new
 #window.set_default_size(200, 100)
 
@@ -38,7 +42,7 @@ error.signal_connect("clicked") {
 }
 
 tooltipURL = Gtk::Tooltips.new
-tooltipURL.set_tip($entry, "Enter youtube URL here.", nil)
+tooltipURL.set_tip($entry, "Enter youtube URL or properly formatted playlist here.", nil)
 
 tooltipDir = Gtk::Tooltips.new
 tooltipDir.set_tip($entryDir, "Enter the directory you wish to save the song to. \nIf it doesnt exist, it will be created", nil)
@@ -49,6 +53,18 @@ table.attach(buttonLogo,0,5,0,4)
 buttonLogoImage = Gtk::Image.new("youtube-logo-transparent_solid.png")
 buttonLogo.image = buttonLogoImage
 
+def fileOperation(file,flag,string)
+        if flag == "w"
+               open(file,"w") do |line|
+                     line.puts string
+               end
+        elsif flag == "r"
+               open(file,"r").each_line do |line|
+                     puts line
+               end       
+        end
+end
+
 def on_error
         md = Gtk::MessageDialog.new(nil, Gtk::Dialog::MODAL | 
              Gtk::Dialog::DESTROY_WITH_PARENT, Gtk::MessageDialog::ERROR, 
@@ -58,22 +74,25 @@ def on_error
 end
 
 def delete_text
+       if !File.exists?("#{$baseDir}/.youtube_dl-gtk")
+               Dir::mkdir "#{$baseDir}/.youtube_dl-gtk"
+       end
+
+       fileOperation("#{$baseDir}/.youtube_dl-gtk/history","w","#{$entry.text}") 
        $entry.delete_text(0,$entry.text.length)
 end
 
 def download(dir,entry,val)
        if val == 1
-              `youtube-dl --extract-audio --audio-format mp3 -o "#{dir}/%(title)s.%(ext)s" "#{entry}/%(title)s.%(ext)s"`
+              `youtube-dl --prefer-avconv --extract-audio --audio-format mp3 -o "#{dir}/%(title)s.%(ext)s" "#{entry}/%(title)s.%(ext)s"`
        elsif val == 0
-              `youtube-dl --extract-audio --audio-format mp3 -o "#{dir}/#{dir}/%(title)s.%(ext)s" "#{entry}"`
+              `youtube-dl --prefer-avconv --extract-audio --audio-format mp3 -o "#{dir}/#{dir}/%(title)s.%(ext)s" "#{entry}"`
        end
 end
 
 button = Gtk::Button.new("Download")
 table.attach(button,5,6,5,6)
-button.signal_connect("clicked") {
-   $baseDir  = ENV['HOME']
-   $musicDir = "#{$baseDir}/Music"
+button.signal_connect("clicked") do
 
        if File.exists?($musicDir) && $entryDir.text.empty? && !$entry.text.empty?
               #`youtube-dl --extract-audio --audio-format mp3 -o "#{$musicDir}/%(title)s.%(ext)s" "#{$entry.text}/%(title)s.%(ext)s"`
@@ -99,7 +118,7 @@ button.signal_connect("clicked") {
               puts "Entry is empty."
               on_error
        end
-}
+end
 
 table.show
 $entry.show
